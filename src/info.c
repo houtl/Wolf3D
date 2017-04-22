be417e926513cf4b6d378f4dfb00c15d47b69ad5
Tianle HOU <thou@e3r9p2.42.fr>
2017-04-22 17:40:42 +0200

    av

--- src/info.c
+++ src/info.c
@@ -1,42 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   wolf3d.h                                           :+:      :+:    :+:   */
+/*   info.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thou <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/22 15:42:05 by thou              #+#    #+#             */
-/*   Updated: 2017/04/22 17:07:02 by thou             ###   ########.fr       */
+/*   Created: 2017/04/22 17:01:00 by thou              #+#    #+#             */
+/*   Updated: 2017/04/22 17:25:39 by thou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WOLF3D_H
-# define WOLF3D_H
+#include "wolf3d.h"
 
-#include "mlx.h"
-#include "libft.h"
-#include "mlx_key.h"
-#include <math.h>
-
-typedef struct	s_all
+void	ft_error(char *str)
 {
-	void		*mlx;
-	void		*win;
-	void		*img;
-	char		**file;
-	char		*date;
-	int			color;
-	int			bpp;
-	int			sl;
-	int			ed;
-	int			pos_x;
-	int			pos_y;
-}				t_a;
-
-/*
-**				info.c
-*/
-
-void			ft_error(char *str);
-
-#endif
+	ft_printf(str);
+	exit(0);
+}
--- src/wolf3d.c
+++ src/wolf3d.c
@@ -6,13 +6,24 @@
 /*   By: thou <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/22 16:25:37 by thou              #+#    #+#             */
-/*   Updated: 2017/04/22 16:26:02 by thou             ###   ########.fr       */
+/*   Updated: 2017/04/22 17:40:13 by thou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
-int main()
+int main(int ac, char **av)
 {
+	int	i;
+	t_a	a;
 
+	if (ac < 2)
+		ft_error("\033[31;1m: Please specify at least one level map file\n"
+				"\033[32;1mUsage: ./wolf3d mapfile0 [mapfile1 [...]]\n");
+	i = ac;
+	a.file = (char**)malloc(sizeof(char*) * i);
+	a.file[i - 1] = "";
+	while (--i > 1)
+		a.file[i - 1] = ft_strdup(av[i]);
+	return (0);
 }
