3f2d96980e73f8e9445c4494de829969a1057109
Tianle HOU <thou@e3r6p1.42.fr>
2017-05-03 18:26:13 +0200

    check

--- src/wolf3d.c
+++ src/wolf3d.c
@@ -6,32 +6,58 @@
 /*   By: thou <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/22 16:25:37 by thou              #+#    #+#             */
-/*   Updated: 2017/05/02 18:20:10 by thou             ###   ########.fr       */
+/*   Updated: 2017/05/03 18:26:07 by thou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
-int ft_checkfile(t_a *a)
+static void ft_checkfile(int fd)
 {
-	int i;
-	int j;
+	int		i;
+	int		x;
+	char	*line;
+	int		len;
+	int		last;
+
+	i = 0;
+	x = 0;
+	while ((get_next_line(fd, &line)) > 0)
+	{
+		i++;
+		if (i == 1)
+			if ((len = ft_strlen(line)) < 3 || ft_stricmp(line, "1") == 0)
+				ft_error("file invalide");
+		else if (i > 1)
+			if (len != ft_strlen(line) || ft_stricmp(line, "01x") == 0 ||
+					line[0] != '1' || line[ft_strlen(line) - 1] != 1)
+				ft_error("file invalide");
+		if (ft_stricmp(line, "x"))
+			x = 1;
+		last = (ft_stricmp(line, "1") == 0) ? 0 : 1;
+		free(line);
+	}
+	if (x != 1 || last != 1)
+		ft_error("file invalide");
+}
+
+static void	ft_checkfiles(t_a *a)
+{
+	int		i;
 
 	i = 0;
 	while (a->file[i])
 	{
-		j = 0;
 		a->fd = open(a->file[i], O_RDONLY);
-		while (get_next_line(a->fd, &line) > 0)
-		{
-			j++;
-			if (
+		ft_checkfile(fd);
+		close(fd);
+	}
+}
 
-
-int main(int ac, char **av)
+int 		main(int ac, char **av)
 {
-	int	i;
-	t_a	a;
+	int		i;
+	t_a		a;
 
 	if (ac < 2)
 		ft_error("\033[31;1m: Please specify at least one level map file\n"
@@ -41,7 +67,7 @@ int main(int ac, char **av)
 	a.file[i - 1] = NULL;
 	while (--i > 1)
 		a.file[i - 1] = ft_strdup(av[i]);
-	ft_checkfile(&a);
+	ft_checkfiles(&a);
 	if (!(a.mlx = mlx_init()))
 		ft_error("mlx_init error");
 	if (!(a.win = mlx_new_window(a.mlx, WIDTH, HEIGHT, "WOLF3D")))
